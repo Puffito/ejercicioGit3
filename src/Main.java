f23b91c64f993fa3e2afb5409d5f58fcd175bdf7
Puffito <puffitored@gmail.com>
2022-11-21 16:55:25 +0100

    bug corregido y testeado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("A las muy buenas");
+        System.out.println("Nuevo nombre");
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String nombre = br.readLine();
     }
 }
